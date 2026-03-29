429424c44f8c60220cf352ed94ea5326a7e4660d
Mayushamihisandali <mayushamihisandali2002@gmail.com>
2026-03-30 03:27:58 +0530

    complete

---
+++ patient-service/src/models/patient.model.js
@@ -0,0 +1,11 @@
+// src/models/patient.model.js
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, enum: ['Male', 'Female', 'Other'], required: true },
+  disease: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Patient', patientSchema);
