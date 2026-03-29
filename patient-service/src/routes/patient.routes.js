2f9f74d87319f9c6a608fcb39daf32b5c7bd4cd2
Mayushamihisandali <mayushamihisandali2002@gmail.com>
2026-03-30 03:28:47 +0530

    complete

---
+++ patient-service/src/routes/patient.routes.js
@@ -0,0 +1,12 @@
+// src/routes/patient.routes.js
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controllers/patient.controller');
+
+router.post('/', patientController.createPatient);
+router.get('/', patientController.getAllPatients);
+router.get('/:id', patientController.getPatientById);
+router.put('/:id', patientController.updatePatient);
+router.delete('/:id', patientController.deletePatient);
+
+module.exports = router;
